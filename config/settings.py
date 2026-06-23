4cdd4bba6671024c6c276a6ee274650e76932d07
recently_was_good <ustinovlev039@gmail.com>
2026-06-23 12:12:19 +1000

    Fixxing mistake

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,10 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f17!obz-%!ive_wpe47vj6!xx6ipjsfkak$&qt=6bgalpw+@_0'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media"
+MEDIA_ROOT = BASE_DIR / "media"
